fc88ea80efb0e5992bd77862ee6a50333a4fe113
codeforests <likunsolutions@gmail.com>
2025-04-24 06:51:51 +0800

    update settings

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--(!wzsa#v^qmzr8!%udjw#a!dmh$qp@=!uh@)$q3gj)2)0qxu^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '68.183.237.218']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
